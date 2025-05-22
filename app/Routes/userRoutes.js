f9d9ea366cf2da75f27c365d2bbc093e171d763d
FerdaoussCHAKROUN <71392449+FerdaoussCHAKROUN@users.noreply.github.com>
2025-05-22 17:11:13 +0200

    add filter

--- app/Routes/userRoutes.js
+++ app/Routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../Controllers/userController");
+const {
+    registerUser,
+    loginUser,
+    getUsers
+  } = require("../Controllers/UserController");
+
 const router = express.Router();
+const authMiddleware = require("../Middleware/authMiddleware");
 
 // POST /users/register
 router.post("/register", registerUser);
 // POST /users/login
 router.post("/login", loginUser);
+
+// Protéger cette route avec le middleware
+router.get("/all", authMiddleware, getUsers);
 
 module.exports = router;
